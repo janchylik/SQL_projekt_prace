cf7d87fa3be317011310a4559ecafe08c0f29d20
Jan Chylík <hchylik77@gmail.com>
2025-11-21 21:37:36 +0100

    push

---
+++ SQL_projekt/2_potrebne_dovednosti.sql
@@ -0,0 +1,28 @@
+
+WITH top_placene_prace AS (
+    SELECT
+        job_id,
+        job_title,
+        company_dim.name AS company_name,
+        salary_year_avg
+    FROM
+        job_postings_fact
+    LEFT JOIN company_dim
+        ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        job_location = 'Anywhere' AND
+        salary_year_avg IS NOT NULL
+    ORDER BY 
+        salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT 
+    top_placene_prace.*,
+    skills
+FROM top_placene_prace
+INNER JOIN skills_job_dim ON skills_job_dim.job_id = top_placene_prace.job_id
+INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+ORDER BY
+    salary_year_avg DESC
